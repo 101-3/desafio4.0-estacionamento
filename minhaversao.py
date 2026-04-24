4278542e594df647eca3433675937c99403ba5f3
Harvei <jhonatagamapescaria@gmail.com>
2026-04-24 15:58:36 -0300

    op3-5/10
    consegui fazer autorizacao com base se a placa está autorizada ou nao

--- minhaversao.py
+++ minhaversao.py
@@ -5,12 +5,20 @@ print("""
       1- Registrar um novo veículo
       2- Listar os veículos registrados
       3- Registrar entrada de um veículo
+      4- Registrar saída de um veículo
       5- Consultas
       6- Mostrar vagas 
       """)
 op = 0
+va = 0
+vp = 0
+vc = 0
 veiculos = []
 estacinamento = []
+alunoestacionamento = {}
+professorestacionamento = {}
+colaboradorestacionamento = {}
+autorizado = False
 while True:
   dados = {}
   op = int(input("Digite a opção desejada: "))
@@ -34,5 +42,23 @@ while True:
        print(f"Modelo: {veiculos[i]['modelo']}")
        print(f"Perfil: {veiculos[i]['perfil']}")
        print(f"Cor: {veiculos[i]['cor']}\n")
+  
+  elif op == 3:
+    placa = input("Digite a placa do veículo: ")
+    for veiculo in veiculos:
+      cont = 0
+      cont += 1
+      if veiculo['placa'] == placa:
+        autorizado = True
+        print("Entrada registrada com sucesso!\n")
+        va += 1
+        break
+      else: 
+       print('', end="")
+      if cont == len(veiculos) and autorizado == False:
+         print("Veículo não registrado. Por favor, registre o veículo antes de registrar a entrada")
+         break
+  
 
 
+      
